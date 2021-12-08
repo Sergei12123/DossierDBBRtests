617ed475c2486291e8629284a02f245c0984e0f1
sealivanov <sealivanov@phoenixit.ru>
2021-12-08 12:15:44 +0300

    Initial commit

---
+++ src/main/java/at/steps/PreconditionSteps.java
@@ -0,0 +1,9 @@
+package at.steps;
+import io.cucumber.java.ru.Пусть;
+
+public class PreconditionSteps {
+    @Пусть("существует пользователь {0}")
+    public void isUserExist(String alias){
+
+    }
+}
